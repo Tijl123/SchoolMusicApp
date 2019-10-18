62621230caaeb77ad322481d2fb0a875e2d3d7e1
Tijl Ingelaere <tijl.ingelaere@gmail.com>
2019-10-18 11:37:22 +0200

    Configuration edge service, not including models and controller

--- MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/MusicEdgeServiceApplication.java
+++ MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/MusicEdgeServiceApplication.java
@@ -1,10 +1,23 @@
 package be.thomasmore.MusicEdgeService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MusicEdgeServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() { return new RestTemplate(); }
+
+	@Bean
+	public ObjectMapper getObjectMapper() { return new ObjectMapper(); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicEdgeServiceApplication.class, args);
