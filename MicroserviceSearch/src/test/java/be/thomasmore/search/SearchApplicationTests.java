f60fb6c26e9425abc98847c436d1c0a3ff136684
HoLLy <dario.db@hotmail.com>
2019-10-18 10:50:58 +0200

    Add search microservice

---
+++ MicroserviceSearch/src/test/java/be/thomasmore/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
