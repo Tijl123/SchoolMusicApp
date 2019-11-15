641cb2f1366f3b29a0eaadf3402f276896e027fc
HoLLy <dario.db@hotmail.com>
2019-11-15 11:17:40 +0100

    Include artists in album response

--- MicroServiceSearch/src/main/java/be/thomasmore/search/entity/Album.java
+++ MicroServiceSearch/src/main/java/be/thomasmore/search/entity/Album.java
@@ -1,20 +1,26 @@
 package be.thomasmore.search.entity;
 
 import be.thomasmore.search.musicbrainz.models.Release;
+import be.thomasmore.search.musicbrainz.models.ReleaseArtistCredit;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Data
 public class Album {
     @Id
     private UUID id;
     private String title;
-    // TODO: artists, tracks
+    private List<Artist> artists;
+    // TODO: tracks
 
     public Album(Release rel) {
         id = rel.getID();
         title = rel.getTitle();
+        artists = Arrays.stream(rel.getArtistCredit()).map(ReleaseArtistCredit::getArtist).map(Artist::new).collect(Collectors.toList());
     }
 }
