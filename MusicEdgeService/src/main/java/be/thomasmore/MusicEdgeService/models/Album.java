56bbe803dd509fb247481cd2e8f056afd76b78f0
Tomas Marlein <tomas.marlein@gmail.com>
2019-12-05 18:51:10 +0100

    models album and artist

--- MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Album.java
+++ MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Album.java
@@ -10,4 +10,46 @@ public class Album {
     private int artistId;
     private Date releaseDate;
     private String albumCoverUrl;
+
+    public Album(String title, int artistId, Date releaseDate, String albumCoverUrl) {
+        this.title = title;
+        this.artistId = artistId;
+        this.releaseDate = releaseDate;
+        this.albumCoverUrl = albumCoverUrl;
+    }
+
+    public Album() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getAlbumCoverUrl() {
+        return albumCoverUrl;
+    }
+
+    public void setAlbumCoverUrl(String albumCoverUrl) {
+        this.albumCoverUrl = albumCoverUrl;
+    }
 }
--- MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Artist.java
+++ MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Artist.java
@@ -6,4 +6,28 @@ import lombok.Data;
 public class Artist {
     private String name;
     private String imageUrl;
+
+    public Artist(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public Artist() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
