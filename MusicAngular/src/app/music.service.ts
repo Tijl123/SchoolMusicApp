ec9fe7b24a9353a3b23847a933a42d0714eb3ed0
HoLLy <dario.db@hotmail.com>
2019-12-05 23:27:43 +0100

    Make zuul gateway work

--- MusicAngular/src/app/music.service.ts
+++ MusicAngular/src/app/music.service.ts
@@ -11,34 +11,36 @@ import { Album } from './models/album.model';
 })
 export class MusicService {
 
+  private apiBase = 'http://localhost:8080/music/';
+
   constructor(private http: HttpClient) { }
 
   getTrackWhereTrackID(trackID: string): Observable<Track> {
-    return this.http.get<Track>("http://localhost:8050/music/trackId/" + trackID);
+    return this.http.get<Track>(this.apiBase + 'trackId/' + trackID);
   }
 
   getTracksWhereTitle(track: string): Observable<Track[]> {
-    return this.http.get<Track[]>("http://localhost:8050/music/track/" + track);
+    return this.http.get<Track[]>(this.apiBase + 'track/' + track);
   }
 
   getAlbumWhereAlbumID(albumID: string): Observable<Album> {
-    return this.http.get<Album>("http://localhost:8050/music/albumId/" + albumID);
+    return this.http.get<Album>(this.apiBase + 'albumId/' + albumID);
   }
 
   getAlbumsWhereTitle(album: string): Observable<Album[]> {
-    return this.http.get<Album[]>("http://localhost:8050/music/album/" + album);
+    return this.http.get<Album[]>(this.apiBase + 'album/' + album);
   }
 
   getArtistWhereArtistID(artistID: string): Observable<Artist> {
-    return this.http.get<Artist>("http://localhost:8050/music/artistId/" + artistID);
+    return this.http.get<Artist>(this.apiBase + 'artistId/' + artistID);
   }
 
   getArtistsWhereName(artist: string): Observable<Artist[]> {
-    return this.http.get<Artist[]>("http://localhost:8050/music/artist/" + artist);
+    return this.http.get<Artist[]>(this.apiBase + 'artist/' + artist);
   }
 
   getLyricsWhereTrackID(trackId: string): Observable<Lyrics> {
-    return this.http.get<Lyrics>("http://localhost:8050/music/lyrics/" + trackId);
+    return this.http.get<Lyrics>(this.apiBase + 'lyrics/' + trackId);
   }
 
 }
