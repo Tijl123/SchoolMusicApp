e26ee054a9ef392c0b1653b6ace553f6c3823670
Tomas Marlein <tomas.marlein@gmail.com>
2019-12-05 18:48:09 +0100

    models edge service

--- MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Lyrics.java
+++ MusicEdgeService/src/main/java/be/thomasmore/MusicEdgeService/models/Lyrics.java
@@ -5,4 +5,38 @@ public class Lyrics {
     private int id;
     private String lyrics;
     private int trackId;
+
+
+    public Lyrics(int id, String lyrics, int trackId) {
+        this.id = id;
+        this.lyrics = lyrics;
+        this.trackId = trackId;
+    }
+
+    public Lyrics() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
 }
